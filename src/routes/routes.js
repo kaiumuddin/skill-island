8c022de1fcf7f2e3918b7201a2082909475ef7d4
kaiumuddin <kaium.uddin2909@gmail.com>
2022-10-26 14:27:45 +0600

    router added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,49 @@
+import {createBrowserRouter} from "react-router-dom";
+import Blog from "../components/Blog";
+import Courses from "../components/Courses";
+import ErrorPage from "../components/ErrorPage";
+import FAQ from "../components/FAQ";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Main from "../layout/Main";
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+        ]
+    }
+]);
+
+export default router;
