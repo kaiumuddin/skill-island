dce4479b728d1ae3f4714d9cb93b3a6a8aa4e82b
kaiumuddin <kaium.uddin2909@gmail.com>
2022-10-27 01:01:33 +0600

    tooltip done'

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,19 +1,18 @@
 import React, {useContext} from 'react';
 import {AuthContext} from "../contexts/UserContext";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
+
 
 const Tooltip = () => {
     const {user} = useContext(AuthContext);
+    const {displayName} = user;
 
     return (
-        <div>
-            <div id="tooltip-jese" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-black bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                Jese Leos
-                <div className="tooltip-arrow" data-popper-arrow></div>
-            </div>
+        <Tippy content={displayName}>
             <img data-tooltip-target="tooltip-jese" className="w-10 h-10 rounded-full" src={user.photoURL} alt="" />
-        </div>
+        </Tippy>
     );
 };
 
 export default Tooltip;
-
