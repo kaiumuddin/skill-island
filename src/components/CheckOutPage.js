6f7754d4ec08dc3b199dcda39bf63e8397b9ffb7
kaiumuddin <kaium.uddin2909@gmail.com>
2022-10-26 22:20:56 +0600

    checkout page done

--- src/components/CheckOutPage.js
+++ src/components/CheckOutPage.js
@@ -6,10 +6,9 @@ const CheckOutPage = () => {
     const {name} = singleCourse;
 
     return (
-        <section className="py-6 dark:bg-gray-800 dark:text-gray-50" >
+        <section className="py-6 bg-white dark:bg-gray-800 dark:text-gray-50" >
             <div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between" >
                 <h1 className="text-3xl font-semibold leading-tight text-center lg:text-left" >{name}</h1>
-                <button className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900" >Contact</button>
             </div>
         </section>
     );
--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useLoaderData} from "react-router-dom";
+import {Link, useLoaderData} from "react-router-dom";
 
 const CourseDetail = () => {
 
@@ -41,7 +41,7 @@ const CourseDetail = () => {
                             <p className="leading-snug" >Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
                         </div>
                     </div>
-                    <button type="button" className="px-8 py-3 font-semibold rounded-full bg-blue-300  dark:bg-gray-100 dark:text-gray-800" >Rounded</button>
+                    <Link to={`/checkout/${id}`} type="button" className="px-8 py-3 font-semibold rounded-full bg-blue-300  dark:bg-gray-100 dark:text-gray-800" >Rounded</Link>
                 </div>
                 <div className="lg:w-1/2 xl:w-3/5 dark:bg-gray-800" >
                     <div className="flex items-center justify-center p-4 md:p-8 lg:p-12" >
