616c6a731b95e843d2d8048eaeec4f52e251d09d
kaiumuddin <kaium.uddin2909@gmail.com>
2022-10-26 16:04:05 +0600

    login and register design done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
 
     return (
 
-        <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900  border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white shadow-md px-2 sm:px-4 py-2.5 dark:bg-gray-900  border-b border-gray-200 dark:border-gray-600">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <Link href="https://flowbite.com/" className="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -21,7 +21,7 @@ const Navbar = () => {
                 </Link>
                 <div className="flex md:order-2 gap-2">
                     <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Logout</button>
-                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                    <Link to='/login' type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</Link>
                     <Tooltip></Tooltip>
                     <button onClick={handleExpand} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
